7c22270bd33a6c28e008c4e3b532b18d9aea0a6e
Shikhar Bhushan <shikhar@schmizz.net>
2009-04-13 03:08:38 +0000

    git-svn-id: http://ncclient.googlecode.com/svn/trunk@12 6dbcf712-26ac-11de-a2f3-1373824ab735

--- src/session/ssh.py
+++ src/session/ssh.py
@@ -13,23 +13,28 @@ class SSH(Session):
     def __init__(self, loadKnownHosts=True, hostname=None, port=22, authType=None, authInfo=None):
         Session.__init__(self)
         self._client = paramiko.SSHClient()
-        self._client.load_system_host_keys()
+        if loadKnownHosts:
+            self._client.load_system_host_keys()
         self._channel = None
-        self._buffer = ''
-        self._send_buffer
+        self._callback = None
+        self._inBuf = ''
+        self._outBuf = ''
     
     def _connect(self):
-        pass
+        self._callback = None #smthn
+        self._outQ.add(hello)
     
     def _remote_closed(self):
-        pass
+        if not self._expectingClose:
+            raise SomeException
     
     def _this_just_in(self, data):
-        buffer += data
-        pos = buffer.find(DELIM)
+        self._inBuf += data
+        pos = self._inBuf.find(DELIM)
         if pos != -1:
-            cb(data[:pos])
-            buffer = buffer[pos + len(DELIM)]
+            msg = self._inBuf[:pos]
+            self._inBuf = self._inBuf[(pos + len(DELIM)):]
+            self._callback(msg)
     
     def connect(self):
         self._connect()
@@ -38,7 +43,6 @@ class SSH(Session):
         Session.connect(self)
     
     def run(self):
-        item = None
         sock = self._channel
         sock.setblocking(0)
         outQ = self._outQ
@@ -46,15 +50,15 @@ class SSH(Session):
             (r, w, e) = select([sock], [sock], [], 60)
             if w:
                 if not outQ.empty():
-                    to_send += ( outQ.get() + DELIM )
-                if to_send:
-                    to_send = to_send[sock.send(to_send):]
+                   self._outBuffer += ( outQ.get() + DELIM )
+                if self._outBuffer:
+                    n = sock.send(self._outBuffer)
+                    self._outBuffer = self._outBuffer[n:]
             if r:
                 data = sock.recv(BUFSIZE)
                 if data:
                     self._this_just_in(data)
                 else:
-                    self._remote_closed()                    
+                    self._remote_closed()
                     
-class MissingHostKeyPolicy(paramiko.MissingHostKeyPolicy):
-    pass
+class MissingHostKeyPolicy(paramiko.MissingHostKeyPolicy): pass
