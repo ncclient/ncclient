efed7d4cd5ac60cbb7c1c38646a6d6dfb711acc9
Shikhar Bhushan <shikhar@schmizz.net>
2009-04-26 03:47:06 +0000

    rpc work thus far
    git-svn-id: http://ncclient.googlecode.com/svn/trunk@64 6dbcf712-26ac-11de-a2f3-1373824ab735

--- ncclient/operations/__init__.py
+++ ncclient/operations/__init__.py
@@ -17,22 +17,20 @@
 import logging
 logger = logging.getLogger('ncclient.operations')
 
-#from ncclient.session import CAPABILITIES
-#
 #from retrieve import Get, GetConfig
 #from edit import EditConfig, DeleteConfig
-#from session import CloseSession, KillSession
-#from lock import Lock, Unlock
+from session import CloseSession, KillSession
+from lock import Lock, Unlock
 #from notification import CreateSubscription
-#
-#__all__ = [
+
+__all__ = [
 #    'Get',
 #    'GetConfig',
 #    'EditConfig',
 #    'DeleteConfig',
 #    'Lock',
 #    'Unlock',
-#    'CloseSession',
-#    'KillSession',
+    'CloseSession',
+    'KillSession',
 #    'CreateSubscription',
 #    ]
--- ncclient/operations/session.py
+++ ncclient/operations/session.py
@@ -14,8 +14,37 @@
 
 'Session-related NETCONF operations'
 
+from ncclient.content.parsers import RPCParser
+from rpc import RPC
+
+
 class CloseSession(RPC):
-    pass
+    
+    def __init__(self):
+        RPC.__init__(self)
+        self.spec = { 'tag': 'close-session' }
+    
+    def _response_cb(self, reply):
+        RPC._response_cb(self, reply)
+        if RPCParser.parse_ok(reply):
+            self._listener.expect_close()
+        self._session.close()
+    
+    def request(self, *args, **kwds):
+        self._do_request(spec, *args, **kwds)
+
 
 class KillSession(RPC):
-    pass
+    
+    def __init__(self):
+        RPC.__init__(self)
+        self.spec = {
+            'tag': 'kill-session',
+            'children': [ { 'tag': 'session-id', 'text': None} ]
+            }
+    
+    def request(self, session_id, reply_event=None):
+        if not isinstance(session_id, basestring): # just make sure...
+            session_id = str(session_id)
+        self.spec['children'][0]['text'] = session_id
+        self._do_request(self.spec, reply_event)
