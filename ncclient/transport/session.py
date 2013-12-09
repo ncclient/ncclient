2decca0893e4d960632e03971bcaa9d7b368af5b
Leonidas Poulopoulos <leopoul@noc.grnet.gr>
2013-12-09 17:25:14 +0200

    Temporarily comment merge indicators

--- ncclient/transport/session.py
+++ ncclient/transport/session.py
@@ -12,11 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-<<<<<<< HEAD
-=======
+# <<<<<<< HEAD
+# =======
 import re
 
->>>>>>> juniper
+# >>>>>>> juniper
 from Queue import Queue
 from threading import Thread, Lock, Event
 
