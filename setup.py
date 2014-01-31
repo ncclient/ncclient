9bfdfeb20e7e0ed8849ddb4e8fc2b8502b80ae4e
Leonidas Poulopoulos <leopoul@noc.grnet.gr>
2014-01-31 02:07:12 +0200

    Add PyPI friendly doc. Update setup.

--- setup.py
+++ setup.py
@@ -28,10 +28,13 @@ if sys.version_info.major == 2 and sys.version_info.minor < 6:
     print "Sorry, Python < 2.6 is not supported"
     exit()
 
+with open('README.rst') as file:
+    long_description = file.read()
 
 setup(name='ncclient',
       version='0.4.0',
       description="Python library for NETCONF clients",
+      long_description=long_description,
       author="Shikhar Bhushan, Leonidas Poulopoulos, Ebben Aries",
       author_email="shikhar@schmizz.net, leopoul@noc.grnet.gr, earies@juniper.net",
       url="http://ncclient.grnet.gr/",
@@ -49,6 +52,7 @@ setup(name='ncclient',
                     ],
       license="Apache License 2.0",
       platforms=["Posix; OS X; Windows"],
+      keywords=('NETCONF', 'NETCONF Python client', 'Juniper Optimization', 'Cisco NXOS Optimization'),
       classifiers=[
           'Programming Language :: Python',
           'Topic :: System :: Networking',
