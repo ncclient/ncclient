4678fac80bec05f4ed563a615ed293a79c5cbe8a
Francis Luong (Franco) <franco@definefunk.com>
2014-07-08 19:28:10 -0400

    improved NCElement, added tests, travis

--- setup.py
+++ setup.py
@@ -28,6 +28,11 @@ if sys.version_info[0] == 2 and sys.version_info[1] < 6:
     print "Sorry, Python < 2.6 is not supported"
     exit()
 
+#parse requirements
+req_lines = [line.strip() for line in open("requirements.txt").readlines()]
+install_reqs = list(filter(None, req_lines))
+
+
 setup(name='ncclient',
       version='0.4.1',
       description="Python library for NETCONF clients",
@@ -35,11 +40,7 @@ setup(name='ncclient',
       author_email="shikhar@schmizz.net, leopoul@noc.grnet.gr, earies@juniper.net",
       url="http://ncclient.grnet.gr/",
       packages=find_packages('.'),
-      install_requires=[
-                    "setuptools>0.6",
-                    "paramiko>=1.7.7.1",
-                    "lxml>3.0"
-                    ],
+      install_requires=install_reqs,
       license="Apache License 2.0",
       platforms=["Posix; OS X; Windows"],
       keywords=('NETCONF', 'NETCONF Python client', 'Juniper Optimization', 'Cisco NXOS Optimization'),
