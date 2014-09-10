68c1c02607729d0ff5751aa1cdac72cb5a4fe7d6
Nathan Printz <nkprintz@gmail.com>
2014-09-10 16:02:58 -0500

    confirmed and at_time mutually exclusive
    Added in error checking to make sure that an error is kicked back when
    the user sends both commit confirmed and commit at. Junos cannot accept
    both of these on the same commit.

--- ncclient/operations/third_party/juniper/rpc.py
+++ ncclient/operations/third_party/juniper/rpc.py
@@ -3,6 +3,7 @@ from ncclient.xml_ import *
 from ncclient.operations.rpc import RPC
 from ncclient.operations.rpc import RPCReply
 from ncclient.operations.rpc import RPCError
+from ncclient import NCClientError
 
 class GetConfiguration(RPC):
     def request(self, format='xml', filter=None):
@@ -74,10 +75,12 @@ class Commit(RPC):
 
         *synchronize* Whether we should synch this commit across both Routing Engines
 
-        *at_time* Mutually exclusive with confirmed. The time at which the commit should happen, in either of these two formats:
+        *at_time* Mutually exclusive with confirmed. The time at which the commit should happen. Junos expects either of these two formats:
             A time value of the form hh:mm[:ss] (hours, minutes, and, optionally, seconds)
             A date and time value of the form yyyy-mm-dd hh:mm[:ss] (year, month, date, hours, minutes, and, optionally, seconds)"""
         node = new_ele("commit")
+        if confirmed and at_time is not None:
+            raise NCClientError("'Commit confirmed' and 'commit at' are mutually exclusive.")
         if confirmed:
             self._assert(":confirmed-commit")
             sub_ele(node, "confirmed")
