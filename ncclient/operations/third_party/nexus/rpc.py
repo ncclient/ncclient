38d7092f07884cb2530f95a5dc24ba177bfbe699
Ray Solomon <raybsolomon@gmail.com>
2014-10-15 18:46:59 -0700

    Allow specifying multiple cmd elements

--- ncclient/operations/third_party/nexus/rpc.py
+++ ncclient/operations/third_party/nexus/rpc.py
@@ -4,8 +4,10 @@ from ncclient.xml_ import *
 from ncclient.operations.rpc import RPC
 
 class ExecCommand(RPC):
-    def request(self, cmd):
-        parent_node = etree.Element(qualify('exec-command', NXOS_1_0))
-        child_node = etree.SubElement(parent_node, qualify('cmd', NXOS_1_0))
-        child_node.text = cmd
-        return self._request(parent_node)
+    def request(self, cmds):
+        node = etree.Element(qualify('exec-command', NXOS_1_0))
+
+        for cmd in cmds:
+            etree.SubElement(node, qualify('cmd', NXOS_1_0)).text = cmd
+
+        return self._request(node)
