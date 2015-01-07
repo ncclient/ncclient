53188de2413b25553569f3161f59d58c76b0afc0
yangxufeng.zhao <yangxufeng.zhao@alipay.com>
2015-01-07 11:13:20 +0800

    add H3C support, Support H3C CLI,Action,Get_bulk,Save,Rollback,etc.

--- ncclient/devices/h3c.py
+++ ncclient/devices/h3c.py
@@ -15,14 +15,16 @@ generic information needed for interaction with a Netconf server.
 from ncclient.xml_ import BASE_NS_1_0
 
 from .default import DefaultDeviceHandler
+from ncclient.operations.third_party.h3c.rpc import *
 
-class HuaweiDeviceHandler(DefaultDeviceHandler):
+
+class H3cDeviceHandler(DefaultDeviceHandler):
     """
-    Huawei handler for device specific information.
+    H3C handler for device specific information.
 
     In the device_params dictionary, which is passed to __init__, you can specify
     the parameter "ssh_subsystem_name". That allows you to configure the preferred
-    SSH subsystem name that should be tried on your Huawei switch. If connecting with
+    SSH subsystem name that should be tried on your H3C switch. If connecting with
     that name fails, or you didn't specify that name, the other known subsystem names
     will be tried. However, if you specify it then this name will be tried first.
 
@@ -30,11 +32,22 @@ class HuaweiDeviceHandler(DefaultDeviceHandler):
     _EXEMPT_ERRORS = []
 
     def __init__(self, device_params):
-        super(HuaweiDeviceHandler, self).__init__(device_params)
+        super(H3cDeviceHandler, self).__init__(device_params)
+
+    def add_additional_operations(self):
+        dict = {}
+        dict["get_bulk"] = GetBulk
+        dict["get_bulk_config"] = GetBulkConfig
+        dict["cli"] = CLI
+        dict["action"] = Action
+        dict["save"] = Save
+        dict["load"] = Load
+        dict["rollback"] = Rollback
+        return dict
 
     def get_capabilities(self):
         # Just need to replace a single value in the default capabilities
-        c = super(HuaweiDeviceHandler, self).get_capabilities()
+        c = super(H3cDeviceHandler, self).get_capabilities()
         return c
 
     def get_xml_base_namespace_dict(self):
