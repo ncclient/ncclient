ecb3eed2eb6ea46fa6a1ca6bcbd3f2c368b1ebe3
jedelman8 <jedelman8@gmail.com>
2015-07-06 13:08:55 -0700

    updated method to support system-view commands and cleaned up resposne

--- ncclient/operations/third_party/hpcomware/rpc.py
+++ ncclient/operations/third_party/hpcomware/rpc.py
@@ -4,11 +4,14 @@ from ncclient.operations.rpc import RPC
 
 
 class CLICommand(RPC):
-    def request(self, cmds):
+    def request(self, cmds, cmd_type=None):
         """
-        Single Execution element is permitted.
+        Single Execution/Configuration element is permitted.
+        Execution is used for display commands while
+        Configuration is used for config (system-view) commands
         cmds can be a list or single command
         """
+        NETCONFBASE_C = "{urn:ietf:params:xml:ns:netconf:base:1.0}"
 
         if isinstance(cmds, list):
             cmd = '\n'.join(cmds)
@@ -17,7 +20,23 @@ class CLICommand(RPC):
 
         node = etree.Element(qualify('CLI', BASE_NS_1_0))
 
-        etree.SubElement(node, qualify('Execution',
-                                       BASE_NS_1_0)).text = cmd
+        if cmd_type == 'display':
+            etree.SubElement(node, qualify('Execution',
+                                           BASE_NS_1_0)).text = cmd
+            response = self._request(node)
+            xml_resp = etree.fromstring(response.xml)
+            text = xml_resp.find('.//{0}Execution'.format(NETCONFBASE_C)).text
 
-        return self._request(node)
+        elif cmd_type == 'config':
+            etree.SubElement(node, qualify('Configuration',
+                                           BASE_NS_1_0)).text = cmd
+
+            response = self._request(node)
+            xml_resp = etree.fromstring(response.xml)
+            text = xml_resp.find('.//{0}Configuration'.format(NETCONFBASE_C)).text
+
+        if text:
+            text = text.replace('\n\n\n', '\n')
+            text = text.replace('\n\n', '\n')
+
+        return text
