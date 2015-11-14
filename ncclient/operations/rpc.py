04acbf9827af1d4a66c93d73c8f71dbc9ceb5f6e
Leonidas Poulopoulos <lpoulopoulos@verisign.com>
2015-11-14 08:17:20 +0100

    Minor cap/small fix

--- ncclient/operations/rpc.py
+++ ncclient/operations/rpc.py
@@ -341,7 +341,7 @@ class RPC(object):
                 else:
                     return self._reply
             else:
-                raise TimeoutExpiredError('Ncclient timed out while waiting for the rpc reply.')
+                raise TimeoutExpiredError('ncclient timed out while waiting for an rpc reply.')
 
     def request(self):
         """Subclasses must implement this method. Typically only the request needs to be built as an
