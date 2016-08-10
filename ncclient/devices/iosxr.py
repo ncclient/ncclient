269548d1cf7025d296e2a589139043f8a191bf4f
Einar Nilsen-Nygaard <einarnn@gmail.com>
2016-08-10 19:51:03 +0100

    Revert "Augmenting IOS-XR support"

--- ncclient/devices/iosxr.py
+++ ncclient/devices/iosxr.py
@@ -12,72 +12,26 @@ generic information needed for interaction with a Netconf server.
 
 """
 
-from ncclient.devices.default import DefaultDeviceHandler
-from ncclient.operations.third_party.iosxr.rpc import ExecuteRpc, Get, GetConfiguration
 
+from .default import DefaultDeviceHandler
 
 def iosxr_unknown_host_cb(host, fingerprint):
-    # This will ignore the unknown host check when connecting to IOS-XR devices
-    return True
-
+        #This will ignore the unknown host check when connecting to IOS-XR devices
+        return True
 
 class IosxrDeviceHandler(DefaultDeviceHandler):
-
     """
     Cisco IOS-XR handler for device specific information.
-    """
 
+    """
     def __init__(self, device_params):
         super(IosxrDeviceHandler, self).__init__(device_params)
 
-    def add_additional_operations(self):
-
-        ops = {}
-
-        ops['rpc'] = ExecuteRpc  # for arbitrary XML RPC reqs
-        # get-config type RPC requsts might or might not have the <filter> tag
-        ops['get'] = Get
-        ops['get_config'] = GetConfiguration
-
-        # others seem to respect the standard
-
-        return ops
-
     def add_additional_ssh_connect_params(self, kwargs):
-
-        kwargs['allow_agent'] = False
+        kwargs['allow_agent']   = False
         kwargs['look_for_keys'] = False
         kwargs['hostkey_verify'] = False
         kwargs['unknown_host_cb'] = iosxr_unknown_host_cb
 
     def perform_qualify_check(self):
         return False
-
-    def transform_reply(self):
-
-        # filter rpc-reply, strip namespaces
-        reply = '''<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
-        <xsl:output method="xml" indent="no"/>
-        <xsl:template match="/|comment()|processing-instruction()">
-            <xsl:copy>
-                <xsl:apply-templates/>
-            </xsl:copy>
-        </xsl:template>
-        <xsl:template match="*">
-            <xsl:element name="{local-name()}">
-                <xsl:apply-templates select="@*|node()"/>
-            </xsl:element>
-        </xsl:template>
-        <xsl:template match="@*">
-            <xsl:attribute name="{local-name()}">
-                <xsl:value-of select="."/>
-            </xsl:attribute>
-        </xsl:template>
-        </xsl:stylesheet>
-        '''
-
-        import sys
-        if sys.version < '3':
-            return reply
-        else:
-            return reply.encode('UTF-8')
