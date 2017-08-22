a65b468aa77242f14b7d56ecb5b5bb28749a3f49
Chase Garner <cgarner@128technology.com>
2017-08-22 13:50:05 -0400

    Added "with-defaults" capability for "get" and "get-config" requests
    RFC 6243 defines an optional "with-defaults" parameter for "get" and
    "get-config" requests that determines how a NETCONF server should handle
    retrieving default configuration values. This update adds support for this
    parameter.
    
    - Added "with-defaults" support to Get and GetConfig
    - Added the "with-defaults" capability to the default client
    - Wrote tests for all of the new cases

--- test/unit/operations/test_retrieve.py
+++ test/unit/operations/test_retrieve.py
@@ -6,6 +6,7 @@ import ncclient.manager
 import ncclient.transport
 from ncclient.xml_ import *
 from ncclient.operations import RaiseMode
+from ncclient.operations.errors import MissingCapabilityError
 from xml.etree import ElementTree
 import copy
 
@@ -31,6 +32,48 @@ class TestRetrieve(unittest.TestCase):
         self.assertEqual(call, xml)
 
     @patch('ncclient.operations.retrieve.RPC._request')
+    def test_get_with_defaults(self, mock_request):
+        session = ncclient.transport.SSHSession(self.device_handler)
+        session._server_capabilities = [':with-defaults']
+        obj = Get(session, self.device_handler, raise_mode=RaiseMode.ALL)
+        obj.request(with_defaults='report-all')
+
+        expected_xml = (
+            '<ns0:get xmlns:ns0="{base}" xmlns:ns1="{defaults}">'
+            '<ns1:with-defaults>report-all</ns1:with-defaults>'
+            '</ns0:get>'.format(
+                base=BASE_NS_1_0,
+                defaults=NETCONF_WITH_DEFAULTS_NS
+            )
+        )
+
+        call = mock_request.call_args_list[0][0][0]
+        call = ElementTree.tostring(call)
+        self.assertEqual(call, expected_xml)
+
+    @patch('ncclient.operations.retrieve.RPC._request')
+    def test_get_with_defaults_not_supported(self, mock_request):
+        session = ncclient.transport.SSHSession(self.device_handler)
+        session._server_capabilities = []
+        obj = Get(session, self.device_handler, raise_mode=RaiseMode.ALL)
+        with self.assertRaises(MissingCapabilityError):
+            obj.request(with_defaults='report-all')
+
+    @patch('ncclient.operations.retrieve.RPC._request')
+    def test_with_defaults_valid_options(self, mock_request):
+        session = ncclient.transport.SSHSession(self.device_handler)
+        session._server_capabilities = [':with-defaults']
+        obj = Get(session, self.device_handler, raise_mode=RaiseMode.ALL)
+
+        obj.request(with_defaults='explicit')
+        obj.request(with_defaults='report-all')
+        obj.request(with_defaults='report-all-tagged')
+        obj.request(with_defaults='trim')
+
+        with self.assertRaises(WithDefaultsError):
+            obj.request(with_defaults='invalid-option')
+
+    @patch('ncclient.operations.retrieve.RPC._request')
     def test_get_config(self, mock_request):
         session = ncclient.transport.SSHSession(self.device_handler)
         obj = GetConfig(session, self.device_handler, raise_mode=RaiseMode.ALL)
@@ -42,6 +85,37 @@ class TestRetrieve(unittest.TestCase):
         call = mock_request.call_args_list[0][0][0]
         call = ElementTree.tostring(call)
         self.assertEqual(call, xml)
+
+    @patch('ncclient.operations.retrieve.RPC._request')
+    def test_get_config_with_defaults(self, mock_request):
+        session = ncclient.transport.SSHSession(self.device_handler)
+        session._server_capabilities = [':with-defaults']
+        obj = GetConfig(session, self.device_handler, raise_mode=RaiseMode.ALL)
+        source = 'candidate'
+        obj.request(source, with_defaults='explicit')
+
+        expected_xml = (
+            '<ns0:get-config xmlns:ns0="{base}" xmlns:ns1="{defaults}">'
+            '<ns0:source><ns0:candidate /></ns0:source>'
+            '<ns1:with-defaults>explicit</ns1:with-defaults>'
+            '</ns0:get-config>'.format(
+                base=BASE_NS_1_0,
+                defaults=NETCONF_WITH_DEFAULTS_NS
+            )
+        )
+
+        call = mock_request.call_args_list[0][0][0]
+        call = ElementTree.tostring(call)
+        self.assertEqual(call, expected_xml)
+
+    @patch('ncclient.operations.retrieve.RPC._request')
+    def test_get_config_with_defaults_not_supported(self, mock_request):
+        session = ncclient.transport.SSHSession(self.device_handler)
+        session._server_capabilities = []
+        obj = GetConfig(session, self.device_handler, raise_mode=RaiseMode.ALL)
+        source = 'candidate'
+        with self.assertRaises(MissingCapabilityError):
+            obj.request(source, with_defaults='explicit')
 
     @patch('ncclient.operations.retrieve.RPC._request')
     def test_get_schema(self, mock_request):
