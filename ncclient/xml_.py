548b0306f6b3d7a5a628a7853dbf9f8ecd813cf4
Einar Nilsen-Nygaard <einarnn@gmail.com>
2018-02-23 14:20:11 +0000

    Fixed merge damage.

--- ncclient/xml_.py
+++ ncclient/xml_.py
@@ -219,8 +219,4 @@ new_ele_ns = lambda tag, ns, attrs={}, **extra: etree.Element(qualify(tag,ns), a
 
 sub_ele = lambda parent, tag, attrs={}, **extra: etree.SubElement(parent, qualify(tag), attrs, **extra)
 
-<<<<<<< HEAD
-sub_ele_ns = lambda parent, tag, ns, attrs={}, **extra: etree.SubElement(parent, qualify(tag,ns), attrs, **extra)
-=======
 sub_ele_ns = lambda parent, tag, ns, attrs={}, **extra: etree.SubElement(parent, qualify(tag, ns), attrs, **extra)
->>>>>>> 3457d3e17f32f6c12f47d5e46dfb0d69d84cac2b
