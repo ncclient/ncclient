a7ac610b6b75bb48463063a48acaea51b326bdb9
avangel <attila.vangel@nokia.com>
2018-06-19 15:20:12 +0200

    examples/juniper various improvements: - Old, python 2 style print statements replaced with logging.info() (logging configured to show date, python file name and line number), - Using bigger timeout values for manager.connect(), needed by e.g. Juniper SRX 300 (which I could test with) for some operations, - More meaningful, less confusing/misleading/sloppy variable names, - get-configuration-jnpr.py: fix: the result of get_configuration() used a bit different data structure, - get-xnm-information.py: XML (NCElement) to str conversion fix for python 3, - Some/most PEP 8 code formatting issues were fixed (what I noticed in PyCharm), - Unnecessary user input (raw_input()) removed from the examples where it was present, easier to run them this way (especially multiple times), - Note: some examples may still be broken, did not have time to debug all of them.

--- examples/juniper/pubkey-auth.py
+++ examples/juniper/pubkey-auth.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python
+import logging
+import sys
 
 from ncclient import manager
 
+
 def connect(host, port, user):
     conn = manager.connect(host=host,
-            port=port,
-            username=user,
-            timeout=10,
-            device_params = {'name':'junos'},
-            hostkey_verify=False)
-
+                           port=port,
+                           username=user,
+                           timeout=60,
+                           device_params={'name': 'junos'},
+                           hostkey_verify=False)
 
     result = conn.get_software_information('brief', test='me')
-    print 'Hostname:', result.xpath('software-information/host-name')[0].text
+    logging.info('Hostname: %s', result.xpath('software-information/host-name')[0].text)
+
 
 if __name__ == '__main__':
+    LOG_FORMAT = '%(asctime)s %(levelname)s %(filename)s:%(lineno)d %(message)s'
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO, format=LOG_FORMAT)
+
     connect('router', 22, 'earies')
