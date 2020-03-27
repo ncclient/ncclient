468f10339167b1073488ac4d28c48571d32782b0
ajitdani <42350637+ajitdani@users.noreply.github.com>
2020-03-27 17:46:32 +0000

    Added operations for IOS-XR

--- ncclient/devices/iosxr.py
+++ ncclient/devices/iosxr.py
@@ -14,6 +14,7 @@ generic information needed for interaction with a Netconf server.
 
 
 from .default import DefaultDeviceHandler
+from ncclient.operations.third_party.iosxr.rpc import ExecuteRpc
 
 def iosxr_unknown_host_cb(host, fingerprint):
         #This will ignore the unknown host check when connecting to IOS-XR devices
@@ -35,3 +36,12 @@ class IosxrDeviceHandler(DefaultDeviceHandler):
 
     def perform_qualify_check(self):
         return False
+
+    def add_additional_operations(self):
+        dict = {}
+        dict["rpc"] = ExecuteRpc
+        # dict["get_configuration"] = GetConfiguration
+        # dict["command"] = Command
+        # dict["commit"] = Commit
+        return dict
+    
