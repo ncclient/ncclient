76c116f6eafa9254cb20d0381451b3285bc36512
ajitdani <42350637+ajitdani@users.noreply.github.com>
2020-03-27 18:10:57 +0000

    Create test_rpc.py

---
+++ test/unit/operations/third_party/iosxr/test_rpc.py
@@ -0,0 +1,23 @@
+from ncclient.operations.third_party.juniper.rpc import *
+import json
+import unittest
+from mock import patch
+from ncclient import manager
+import ncclient.manager
+import ncclient.transport
+from ncclient.xml_ import *
+from ncclient.operations import RaiseMode
+from xml.etree import ElementTree
+
+
+class TestRPC(unittest.TestCase):
+
+    @patch('ncclient.transport.SSHSession')
+    @patch('ncclient.operations.third_party.iosxr.rpc.RPC._request')
+    def test_execute_rpc(self, mock_request, mock_session):
+        device_handler = manager.make_device_handler({'name': 'junos'})
+        session = ncclient.transport.SSHSession(device_handler)
+        obj = ExecuteRpc(session, device_handler, raise_mode=RaiseMode.ALL)
+        rpc = new_ele('get-software-information')
+        obj.request(rpc)
+        mock_request.assert_called_once_with(rpc)
