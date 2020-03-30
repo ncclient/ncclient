4310784294aebd0774d5f4a4de49e47f0873b63e
ajitdani <42350637+ajitdani@users.noreply.github.com>
2020-03-30 18:39:09 +0100

    Update test_rpc.py
    Incorporated review comment

--- test/unit/operations/third_party/iosxr/test_rpc.py
+++ test/unit/operations/third_party/iosxr/test_rpc.py
@@ -15,7 +15,7 @@ class TestRPC(unittest.TestCase):
     @patch('ncclient.transport.SSHSession')
     @patch('ncclient.operations.third_party.iosxr.rpc.RPC._request')
     def test_execute_rpc(self, mock_request, mock_session):
-        device_handler = manager.make_device_handler({'name': 'junos'})
+        device_handler = manager.make_device_handler({'name': 'iosxr'})
         session = ncclient.transport.SSHSession(device_handler)
         obj = ExecuteRpc(session, device_handler, raise_mode=RaiseMode.ALL)
         rpc = new_ele('get-software-information')
