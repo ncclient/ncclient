e7ddf98246b3d65a203021c62ad6f280aad00d5b
fredgan <ganshaolong@vip.qq.com>
2020-07-14 15:16:01 +0800

    add two api for getting supported devices info

---
+++ ncclient/devices/__init__.py
@@ -0,0 +1,19 @@
+# supported devices config, add new device (eg: 'device name':'device lable').
+supported_devices_cfg = {'junos':'Juniper',
+                         'csr':'Cisco CSR1000v',
+                         'nexus':'Cisco Nexus',
+                         'iosxr':'Cisco IOS XR',
+                         'iosxe':'Cisco IOS XE',
+                         'huawei':'Huawei',
+                         'huaweiyang':'Huawei',
+                         'alu':'Alcatel Lucent',
+                         'h3c':'H3C',
+                         'hpcomware':'HP Comware',
+                         'default':'Server or anything not in above'}
+
+def get_supported_devices():
+    return tuple(supported_devices_cfg.keys())
+
+def get_supported_device_labels():
+    return supported_devices_cfg
+
