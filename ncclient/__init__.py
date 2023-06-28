6576e365298afd6a22d7f795f7a61564ec275a05
Einar Nilsen-Nygaard <einarnn@gmail.com>
2023-06-28 14:38:57 +0100

    updated versioneer

--- ncclient/__init__.py
+++ ncclient/__init__.py
@@ -23,6 +23,5 @@ class NCClientError(Exception):
     "Base type for all NCClient errors"
     pass
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
