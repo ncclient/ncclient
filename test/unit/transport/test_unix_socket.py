ad88686f4772ae883109a8f97a26708a352b89b8
Einar Nilsen-Nygaard <einarnn@gmail.com>
2024-11-26 09:48:26 +0000

    Only import UnixSocketSession if the platform is not Windows (#609)
    * only import UnixSocketSession if the platform is not Windows
    * add windows automated test
    * updated python versions
    * tweaked pip commands for module installation
    * modified camelCase filename for unix socket test

--- test/unit/transport/test_unix_socket.py
+++ test/unit/transport/test_unix_socket.py
@@ -9,13 +9,16 @@ try:
 except ImportError:
     from mock import MagicMock, patch, call
 
-from ncclient.transport.errors import UnixSocketError
-from ncclient.transport.unixSocket import UnixSocketSession
+# only import on non-Windows platforms
+if sys.platform != 'win32':
+    from ncclient.transport.errors import UnixSocketError
+    from ncclient.transport.unixSocket import UnixSocketSession
 
 PATH = '/tmp/test_socket.sock'
 
 class TestUnixSocket(unittest.TestCase):
 
+    @unittest.skipIf(sys.platform.startswith('win'), "Skipping on Windows")
     @patch('socket.socket.close')
     def test_close_UnixSocket(self, mock_sock_close_fn):
         session = UnixSocketSession(MagicMock())
@@ -25,6 +28,7 @@ class TestUnixSocket(unittest.TestCase):
         mock_sock_close_fn.assert_called_once_with()
         self.assertFalse(session.connected)
 
+    @unittest.skipIf(sys.platform.startswith('win'), "Skipping on Windows")
     @patch('socket.socket')
     @patch('ncclient.transport.Session._post_connect')
     def test_connect_UnixSocket(self, mock_post_connect, mock_socket):
